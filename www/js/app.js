94929f9ba23e042ece820dab89f51280dffbe69d
7idbiz <beshan@bigmir.net>
2014-06-20 03:01:19 +0300

    how it work??

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,47 @@
-function onDeviceReady() {
-//    angular.bootstrap(document, ["appGap"]);
-}
+angular.module('ionicApp', ['ionic'])
 
-document.addEventListener('deviceready', onDeviceReady, false);
-setTimeout(function(){
-    if (window.location.protocol.indexOf('file:')==0) {
-        onDeviceReady();    
-    }
-},200);
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+  .state('intro', {
+    url: '/',
+    templateUrl: 'intro.html',
+    controller: 'IntroCtrl'
+  })
+  .state('main', {
+    url: '/main',
+    templateUrl: 'main.html',
+    controller: 'MainCtrl'
+  });
+
+  $urlRouterProvider.otherwise("/");
+
+})
+
+.controller('IntroCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
+ 
+  // Called to navigate to the main app
+  $scope.startApp = function() {
+    $state.go('main');
+  };
+  $scope.next = function() {
+    $ionicSlideBoxDelegate.next();
+  };
+  $scope.previous = function() {
+    $ionicSlideBoxDelegate.previous();
+  };
+
+  // Called each time the slide changes
+  $scope.slideChanged = function(index) {
+    $scope.slideIndex = index;
+  };
+})
+
+.controller('MainCtrl', function($scope, $state) {
+  console.log('MainCtrl');
+  
+  $scope.toIntro = function(){
+    $state.go('intro');
+  }
+});
+
