94bd1694052d7cd4b05ed70f8541f42392197163
7idbiz <beshan@bigmir.net>
2014-06-20 03:23:52 +0300

    slidemenu

--- www/js/app.js
+++ www/js/app.js
@@ -1,47 +1,77 @@
-angular.module('ionicApp', ['ionic'])
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
 
 .config(function($stateProvider, $urlRouterProvider) {
-
   $stateProvider
-  .state('intro', {
-    url: '/',
-    templateUrl: 'intro.html',
-    controller: 'IntroCtrl'
-  })
-  .state('main', {
-    url: '/main',
-    templateUrl: 'main.html',
-    controller: 'MainCtrl'
-  });
 
-  $urlRouterProvider.otherwise("/");
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "templates/menu.html",
+      controller: 'AppCtrl'
+    })
 
-})
+    .state('app.search', {
+      url: "/search",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/search.html"
+        }
+      }
+    })
 
-.controller('IntroCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
- 
-  // Called to navigate to the main app
-  $scope.startApp = function() {
-    $state.go('main');
-  };
-  $scope.next = function() {
-    $ionicSlideBoxDelegate.next();
-  };
-  $scope.previous = function() {
-    $ionicSlideBoxDelegate.previous();
-  };
+    .state('app.browse', {
+      url: "/browse",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/browse.html"
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: "/playlists",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/playlists.html",
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
 
-  // Called each time the slide changes
-  $scope.slideChanged = function(index) {
-    $scope.slideIndex = index;
-  };
-})
-
-.controller('MainCtrl', function($scope, $state) {
-  console.log('MainCtrl');
-  
-  $scope.toIntro = function(){
-    $state.go('intro');
-  }
+    .state('app.single', {
+      url: "/playlists/:playlistId",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/playlist.html",
+          controller: 'PlaylistCtrl'
+        }
+      }
+    });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
 });
+
+
+
+
+
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,189 +1,22 @@
 
-/*
-angular.module('appGap', ['ionic'])
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  $stateProvider
-  .state('intro', {
-    url: '/',
-    templateUrl: 'intro.html',
-    controller: 'IntroCtrl'
-  })
-  .state('main', {
-    url: '/main',
-    templateUrl: 'main.html',
-    controller: 'MainCtrl'
-  });
-
-  $urlRouterProvider.otherwise("/");
-
-})
 
 
-
-.controller('IntroCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
- 
-  // Called to navigate to the main app
-  $scope.startApp = function() {
-    $state.go('main');
-  };
-  $scope.next = function() {
-    $ionicSlideBoxDelegate.next();
-  };
-  $scope.previous = function() {
-    $ionicSlideBoxDelegate.previous();
-  };
-
-  // Called each time the slide changes
-  $scope.slideChanged = function(index) {
-    $scope.slideIndex = index;
-  };
-})
-
-.controller('MainCtrl', function($scope, $state) {
-  console.log('MainCtrl');
-  
-  $scope.toIntro = function(){
-    $state.go('intro');
-  }
-});
-
-
-*/
-
-
-
-/*
-var appGap = angular.module("appGap", []);
-
-appGap.controller("HomeController", function($scope){
-    $scope.message = "AngularJS!";
-});
-*/
-
-
-/*
-angular.module('appGap', ['ionic', 'ngRoute'])
-
-.config(function ($compileProvider){
-  // Set the whitelist for certain URLs just to be safe
-  $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
-})
-
-.config(function($routeProvider, $locationProvider) {
-  // Set up the initial routes that our app will respond to.
-  // These are then tied up to our nav router which animates and
-  // updates a navigation bar
-  $routeProvider.when('/', {
-    templateUrl: 'intro.html',
-    controller: 'IntroCtrl'
-  });
-
-  $routeProvider.when('/main', {
-    templateUrl: 'intro.html',
-    controller: 'IntroCtrl'
-//    templateUrl: 'main.html',
-//    controller: 'MainCtrl'
-  });
-
-  // if none of the above routes are met, use this fallback
-  // which executes the 'IntroCtrl' controller (controllers.js)
-  $routeProvider.otherwise({
-    redirectTo: '/'
-  });
-});
 
 angular.module('starter.controllers', [])
 
-.controller('IntroCtrl', function($scope, $location) {
-  // Called to navigate to the main app
-  var startApp = function() {
-    $location.path('/main');
-
-    // Set a flag that we finished the tutorial
-    window.localStorage['didTutorial'] = true;
-  };
-
-  // Check if the user already did the tutorial and skip it if so
-  if(window.localStorage['didTutorial'] === "true") {
-    startApp();
-    return;
-  }
-
-  // Move to the next slide
-  $scope.next = function() {
-    $scope.$broadcast('slideBox.nextSlide');
-  };
-
-
-  // Our initial right buttons
-  var rightButtons = [
-    {
-      content: 'Next',
-      type: 'button-positive button-clear',
-      tap: function(e) {
-        // Go to the next slide on tap
-        $scope.next();
-      }
-    }
-  ];
-  
-  // Our initial left buttons
-  var leftButtons = [
-    {
-      content: 'Skip',
-      type: 'button-positive button-clear',
-      tap: function(e) {
-        // Start the app on tap
-        startApp();
-      }
-    }
-  ];
-
-  // Bind the left and right buttons to the scope
-  $scope.leftButtons = leftButtons;
-  $scope.rightButtons = rightButtons;
-
-
-  // Called each time the slide changes
-  $scope.slideChanged = function(index) {
-
-    // Check if we should update the left buttons
-    if(index > 0) {
-      // If this is not the first slide, give it a back button
-      $scope.leftButtons = [
-        {
-          content: 'Back',
-          type: 'button-positive button-clear',
-          tap: function(e) {
-            // Move to the previous slide
-            $scope.$broadcast('slideBox.prevSlide');
-          }
-        }
-      ];
-    } else {
-      // This is the first slide, use the default left buttons
-      $scope.leftButtons = leftButtons;
-    }
-    
-    // If this is the last slide, set the right button to
-    // move to the app
-    if(index == 2) {
-      $scope.rightButtons = [
-        {
-          content: 'Start using MyApp',
-          type: 'button-positive button-clear',
-          tap: function(e) {
-            startApp();
-          }
-        }
-      ];
-    } else {
-      // Otherwise, use the default buttons
-      $scope.rightButtons = rightButtons;
-    }
-  };
+.controller('AppCtrl', function($scope) {
 })
 
-*/
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
