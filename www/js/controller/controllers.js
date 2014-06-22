33ee6335209bfab6374a4279ed310bd52eaddba5
7idbiz <beshan@bigmir.net>
2014-06-22 19:34:59 +0300

    min searchbox

--- www/js/controller/controllers.js
+++ www/js/controller/controllers.js
@@ -94,57 +94,53 @@ appGap.controller('MapPanelController', function($scope, $state) {
         new google.maps.LatLng(-33.8474, 151.2631));
     appGap.gMap.fitBounds(defaultBounds);
 
-    // Create the search box and link it to the UI element.
-    var input = /** @type {HTMLInputElement} */(
-        document.getElementById('pac-input'));
-    appGap.gMap.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-    var searchBox = new google.maps.places.SearchBox(
-        /** @type {HTMLInputElement} */(input));
+    var input = (document.getElementById('pac-input'));
+//    appGap.gMap.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-    // Listen for the event fired when the user selects an item from the
-    // pick list. Retrieve the matching places for that item.
+    var searchBox = new google.maps.places.SearchBox( (input) );
+
     google.maps.event.addListener(searchBox, 'places_changed', function() {
         alert(111);
-        var places = searchBox.getPlaces();
-
-        for (var i = 0, marker; marker = markers[i]; i++) {
-            marker.setMap(null);
-        }
-
-        // For each place, get the icon, place name, and location.
-        markers = [];
-        var bounds = new google.maps.LatLngBounds();
-        for (var i = 0, place; place = places[i]; i++) {
-            var image = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            var marker = new google.maps.Marker({
-                map: appGap.gMap,
-                icon: image,
-                title: place.name,
-                position: place.geometry.location
-            });
-
-            markers.push(marker);
-
-            bounds.extend(place.geometry.location);
-        }
-
-        appGap.gMap.fitBounds(bounds);
-        alert(222);
+//        var places = searchBox.getPlaces();
+//
+//        for (var i = 0, marker; marker = markers[i]; i++) {
+//            marker.setMap(null);
+//        }
+//
+//        // For each place, get the icon, place name, and location.
+//        markers = [];
+//        var bounds = new google.maps.LatLngBounds();
+//        for (var i = 0, place; place = places[i]; i++) {
+//            var image = {
+//                url: place.icon,
+//                size: new google.maps.Size(71, 71),
+//                origin: new google.maps.Point(0, 0),
+//                anchor: new google.maps.Point(17, 34),
+//                scaledSize: new google.maps.Size(25, 25)
+//            };
+//
+//            // Create a marker for each place.
+//            var marker = new google.maps.Marker({
+//                map: appGap.gMap,
+//                icon: image,
+//                title: place.name,
+//                position: place.geometry.location
+//            });
+//
+//            markers.push(marker);
+//
+//            bounds.extend(place.geometry.location);
+//        }
+//
+//        appGap.gMap.fitBounds(bounds);
+//        alert(222);
     });
 
     // Bias the SearchBox results towards places that are within the bounds of the
     // current map's viewport.
-    google.maps.event.addListener(appGap.gMap, 'bounds_changed', function() {
-        var bounds = appGap.gMap.getBounds();
-        searchBox.setBounds(bounds);
-    });
+//    google.maps.event.addListener(appGap.gMap, 'bounds_changed', function() {
+//        var bounds = appGap.gMap.getBounds();
+//        searchBox.setBounds(bounds);
+//    });
 });
